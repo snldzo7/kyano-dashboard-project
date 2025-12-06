b686d2fb57232045968683d706b460101d54e43d
snldzo7 <zouari.dean@gmail.com>
2025-12-06 16:07:51 +0100

    clean up clay-clj and react

--- clay-clj/scripts/start_server.clj
+++
@@ -1,9 +0,0 @@
-(require '[clay.server.mouse-server :as server])
-
-(server/start-server!)
-
-(println "Backend ready - Press Ctrl+C to stop")
-
-;; Keep the process alive
-(while true
-  (Thread/sleep 1000))
